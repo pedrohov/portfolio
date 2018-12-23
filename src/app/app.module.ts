fa62e023a399e98195cdc08c58e45daf96024cb1
Pedro Veloso <pedrohov@users.noreply.github.com>
2018-12-22 22:41:50 -0200

    Improve mobile layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
