47fe1a7ba6c4109115118f84f44a6a5dfbdf874c
Pedro Veloso <pedrohov@users.noreply.github.com>
2018-12-24 18:48:04 -0200

    Fix route anim

--- src/app/animation.ts
+++ src/app/animation.ts
@@ -2,7 +2,31 @@ import { trigger, style, transition, animate, query, stagger, group, animateChil
 
 export const slideInAnimation =
   trigger('routeAnim', [
-    transition('* => Home', [
+    transition('Portfolio => Home', [
+      style({ position: 'relative' }),
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%'
+        })
+      ]),
+      query(':enter', [
+        style({ left: '-100%'})
+      ]),
+      query(':leave', animateChild(), {optional: true}),
+      group([
+        query(':leave', [
+          animate('300ms ease-out', style({ left: '100%'}))
+        ], {optional: true}),
+        query(':enter', [
+          animate('300ms ease-out', style({ left: '0%'}))
+        ], {optional: true})
+      ]),
+      query(':enter', animateChild(), {optional: true}),
+    ]),
+    transition('Description => Home', [
       style({ position: 'relative' }),
       query(':enter, :leave', [
         style({
