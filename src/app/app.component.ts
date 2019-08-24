a8a4eceb058a345cf603d0cabcd2b5d0bec04070
Pedro Hov <pedrohoveloso@gmail.com>
2019-08-23 22:02:39 -0700

    Improve navbar / remove bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,33 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { slideInAnimation } from './animation';
 
-import { trigger, style, transition, animate, query, stagger, group } from '@angular/animations';
+import { trigger, style, transition, animate, query, stagger, group, state } from '@angular/animations';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [ slideInAnimation ]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    animations: [
+        slideInAnimation,
+        trigger('toggleMenu', [
+            state('open', style({ maxHeight: '180px' })),
+            state('closed', style({ maxHeight: '0' })),
+            transition('open <=> closed', [
+                animate('0.3s ease-out')
+            ]),
+        ])
+    ]
 })
 
 export class AppComponent {
-  title = 'portfolio';
+    toggleState : boolean = false;
 
-  prepareRoute(outlet: RouterOutlet) {
-    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
-  }
+    toggle() {
+        this.toggleState = !this.toggleState;
+    }
+
+    prepareRoute(outlet: RouterOutlet) {
+        return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+    }
 }
 
