214d1e0126cf3bbe801ae3ec6c483769a46b2944
Pedro <pedro.veloso@agrosatelite.com.br>
2022-05-26 23:43:28 -0300

    Migrate to angular @12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatIconModule} from '@angular/material/icon';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProjectCardComponent } from './project-card/project-card.component';
-import { TagComponent } from './tag/tag.component';
-import { ProjectDetailsComponent } from './project-details/project-details.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { AboutComponent } from './about/about.component';
-import { TagFilterPipe } from './pipes/tag-filter.pipe';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ProjectCardComponent } from "./project-card/project-card.component";
+import { TagComponent } from "./tag/tag.component";
+import { ProjectDetailsComponent } from "./project-details/project-details.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { AboutComponent } from "./about/about.component";
+import { TagFilterPipe } from "./pipes/tag-filter.pipe";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
   declarations: [
@@ -22,14 +21,10 @@ import { HomeComponent } from './home/home.component';
     PortfolioComponent,
     AboutComponent,
     TagFilterPipe,
-    HomeComponent
+    HomeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
