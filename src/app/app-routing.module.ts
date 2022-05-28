f0d31ceb79cfd3d1631f309632840083d81a70ff
Pedro <pedrohoveloso@gmail.com>
2022-05-27 22:23:45 -0300

    Remove 'portfolio' baseRef

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,32 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { HomeComponent } from './home/home.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { AboutComponent } from './about/about.component';
-import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { HomeComponent } from "./home/home.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { AboutComponent } from "./about/about.component";
+import { ProjectDetailsComponent } from "./project-details/project-details.component";
 
 const routes: Routes = [
-    { path: 'home', component: HomeComponent, data: { animation: 'Home' } },
-    { path: 'projects', component: PortfolioComponent, data: { animation: 'Portfolio' } },
-    { path: 'about', component: AboutComponent, data: { animation: 'About' } },
-    { path: 'projects/:title', component: ProjectDetailsComponent, data: { animation: 'Portfolio' } },
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: '*', redirectTo: '/home', pathMatch: 'full' }
+  { path: "home", component: HomeComponent, data: { animation: "Home" } },
+  {
+    path: "projects",
+    component: PortfolioComponent,
+    data: { animation: "Portfolio" },
+  },
+  { path: "about", component: AboutComponent, data: { animation: "About" } },
+  {
+    path: "projects/:title",
+    component: ProjectDetailsComponent,
+    data: { animation: "Portfolio" },
+  },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "*", redirectTo: "/home", pathMatch: "full" },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', useHash: true })],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { scrollPositionRestoration: "enabled" }),
+  ],
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
