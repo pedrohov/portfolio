44d6b014a9e56df007e0253098d080def62b00c9
Pedro <pedrohoveloso@yahoo.com.br>
2022-06-12 19:55:06 -0300

    Fix portfolio component tests. Add SideProject tests

---
+++ src/app/core/side-project.service.spec.ts
@@ -0,0 +1,25 @@
+import { TestBed } from "@angular/core/testing";
+import { SIDE_PROJECTS } from "./data/side-projects";
+import { SideProjectService } from "./side-project.service";
+
+describe("SideProjectService", () => {
+  let sideProjectService: SideProjectService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [SideProjectService],
+    }).compileComponents();
+
+    sideProjectService = TestBed.inject(SideProjectService);
+  });
+
+  it("should create the SideProject service", () => {
+    expect(sideProjectService).toBeTruthy();
+  });
+
+  it("should fetch all Side Projects", () => {
+    let sideProjects = [];
+    sideProjectService.getSideProjects().subscribe((sp) => (sideProjects = sp));
+    expect(sideProjects).toEqual(SIDE_PROJECTS);
+  });
+});
--- src/app/portfolio/portfolio.component.spec.ts
+++ src/app/portfolio/portfolio.component.spec.ts
@@ -3,15 +3,10 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterTestingModule } from "@angular/router/testing";
 import { PROJECTS } from "@core/data/projects";
-import { TAGS } from "@core/data/tags";
-import { TagService } from "@core/tag.service";
-import { TagComponent } from "@shared/tag/tag.component";
-import { TagFilterPipe } from "../pipes/tag-filter.pipe";
 import { ProjectCardComponent } from "../project-card/project-card.component";
 import { PortfolioComponent } from "./portfolio.component";
 
 describe("PortfolioComponent", () => {
-  let tagService: TagService;
   let component: PortfolioComponent;
   let fixture: ComponentFixture<PortfolioComponent>;
   let element: HTMLElement;
@@ -19,48 +14,16 @@ describe("PortfolioComponent", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [CommonModule, BrowserAnimationsModule, RouterTestingModule],
-      declarations: [
-        PortfolioComponent,
-        TagComponent,
-        ProjectCardComponent,
-        TagFilterPipe,
-      ],
-      providers: [TagService],
+      declarations: [PortfolioComponent, ProjectCardComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PortfolioComponent);
     component = fixture.componentInstance;
     element = fixture.elementRef.nativeElement;
-    tagService = TestBed.inject(TagService);
   });
 
   it("should create the Portfolio", () => {
     expect(component).toBeTruthy();
-  });
-
-  it("should create the TagService", () => {
-    expect(tagService).toBeTruthy();
-  });
-
-  it("should fetch all service Tags", () => {
-    let tags = [];
-    tagService.getTags().subscribe((t) => (tags = t));
-    expect(tags).toEqual(TAGS);
-  });
-
-  it("should have tags", () => {
-    let tags = [];
-    tagService.getTags().subscribe((t) => (tags = t));
-    fixture.detectChanges();
-    expect(component.tags).toEqual(tags);
-  });
-
-  it("should get all selected tags", () => {
-    tagService.getTags().subscribe((t) => (component.tags = t));
-    component.tags[1].selected = false;
-    const selectedTags = component.getSelectedTags();
-    fixture.detectChanges();
-    expect(component.tags.length).toBeGreaterThan(selectedTags.length);
   });
 
   it("should fetch all projects", () => {
