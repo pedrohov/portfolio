b2f3b3e067a01c36e225e77e76c0bb09a2dc06aa
Pedro <pedrohoveloso@yahoo.com.br>
2022-06-16 21:20:43 -0300

    Remove old project detail structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,9 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { HomeComponent } from "./home/home.component";
-import { PortfolioComponent } from "./portfolio/portfolio.component";
 import { ProjectDetailsComponent } from "./project-details/project-details.component";
 
 export const ROUTES: Routes = [
-  {
-    path: "projects",
-    component: PortfolioComponent,
-    data: { animation: "Portfolio" },
-  },
   {
     path: "projects/:title",
     component: ProjectDetailsComponent,
